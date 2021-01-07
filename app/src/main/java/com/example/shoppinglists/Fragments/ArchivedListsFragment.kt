e6819328319c3ba2d0fc085d8ca102f0de729f6d
BlasQu <damianhs138@gmail.com>
2021-01-07 16:19:41 +0100

    init commit

---
+++ app/src/main/java/com/example/shoppinglists/Fragments/ArchivedListsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglists.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.shoppinglists.R
+
+class ArchivedListsFragment : Fragment(R.layout.fragment_archived_lists) {
+}
---
+++ app/src/main/java/com/example/shoppinglists/Fragments/ShoppingListsFragment.kt
@@ -0,0 +1,14 @@
+package com.example.shoppinglists.Fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.shoppinglists.R
+
+class ShoppingListsFragment : Fragment(R.layout.fragment_shopping_lists) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
+
